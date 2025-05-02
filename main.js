b09f55c035a0518db016939858f7fc8ac664857b
picspiii1234 <ibrihim539@gmail.com>
2025-05-02 22:27:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+// let ism = prompt ('Ismingizni kiriting');
+// console.log('ism:' , ism);
+
+// let yoshingiz = prompt ('Yoshingizni kiriting');
+// console.log('yosh:' , yoshingiz);
+
+// let manzil = prompt ('Manzilingizni kiriting');
+// console.log('manzil:' , manzil);
+
+
+// alert('Salom meni ismim' + ism ,
+//     + 'yoshim esa' + yoshingiz + ' shahrida turaman' + manzil );
+
+
+
+let ism = prompt ('Ismingizni kiriting');
+let fml = prompt ('Familiyangizni kiriting');
+let yosh = prompt ('Yoshingizni  kiriting');
+let fan = prompt ('Yoqtirgan vaningiz yozing');
+let ytk = prompt ('Yashaydigan tumaningizni kiritin');
+alert('Malumotlaringizni kiritkaningiz uchun tashakur')
+
+console.log( 'ism:', ism);
+console.log( 'familya:' , fml);
+console.log( 'yosh:'  , yosh);
+console.log(  'yahshi korgan fan:' , fan);
+console.log(  'yashash tuman:' , ytk);
